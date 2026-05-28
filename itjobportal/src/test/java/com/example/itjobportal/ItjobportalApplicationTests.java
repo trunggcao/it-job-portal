268fa04b165f33f5b27f2d6023f6d9c99f963378
trunggcao <caoquoctrung2004@gmail.com>
2026-05-29 04:15:22 +0700

    first commit

---
+++ itjobportal/src/test/java/com/example/itjobportal/ItjobportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itjobportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItjobportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
